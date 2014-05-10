ade55098f2351a1d87062f6c69e40a396ff3303f
Calvin Woo <calvin.woo@citrix.com>
2014-05-09 17:58:57 -0700

    role revenue txt

---
+++ dbquery.py
@@ -0,0 +1,47 @@
+from pymongo import MongoClient
+
+
+client = MongoClient('localhost', 27017)
+db = client.forresterDB
+reports = db.reports
+
+roles = {
+    'Analyst Relations': [],
+    'Application Development & Delivery': [],
+    'Business Process': [],
+    'CIO': [],
+    'Consumer Product Strategy': [],
+    'Content & Collaboration': [],
+    'CMO': [],
+    'Customer Experience': [],
+    'Customer Insights': [],
+    'eBusiness & Channel Strategy': [],
+    'Enterprise Architecture': [],
+    'Infrastructure & Operations': [],
+    'Marketing Leadership': [],
+    'Sales Enablement': [],
+    'Security & Risk': [],
+    'Sourcing & Vendor Management': []
+}
+
+for report in reports.find():
+    role = report['role']
+    if role is not None:
+        revenue = report['revenue']
+        if revenue is not None:
+            roles[role].append(report['revenue'])
+
+averages = {}
+for role, revenues in roles.items():
+    count = len(revenues)
+    total = 0.0
+    for revenue in revenues:
+        total = total + revenue
+    average = total / count
+    averages[role] = average
+print averages
+
+f = open('rolerevenue.txt', 'w')
+for role, average in averages.items():
+    f.write('role: '+role+'\t \t'+'average: '+str(average)+'\n')
+f.close()
